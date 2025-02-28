7ced7095d1ea5344e1677b1fa1b7e7721a251b6d
whoisjoshua <joshua.zeng462@gmail.com>
2025-02-27 20:09:41 -0800

    Added barebones selenium structure

--- kkengine.py
+++ kkengine.py
@@ -4,16 +4,22 @@
 """
 kkengine.py
 
+This script fearlessly navigates the chaotic digital jungle of restaurant reservations,  
+battling captchas, dodging 404 errors, and securing that sacred dinner spot  
+before Karen from accounting takes the last table. 
 
+Note that this is not used for any distribution of service denial of any kind and is only 
+used to run locally on one machine. If anyone chooses to use this, please be considerate 
+of businesses. Any improper use of this script is not our responsibility.
 
-Author: Joshua Zeng; Ethan Lowe
+Authors: Joshua Zeng; Ethan Lowe
 Version: 1.0.0
 """
 
 import argparse
 from selenium import webdriver
 
-VERBOSE = false
+VERBOSE = False
 
 def get_webdriver(browser):
     """Returns the appropriate WebDriver based on the selected browser."""
@@ -31,7 +37,13 @@ def main():
                         help="Choose the browser: 'chrome' or 'firefox'")
     parser.add_argument("--url", type=str, default="https://kichikichi.com/kichikichi-reservation/",
                         help="Specify the website URL to open")
+    parser.add_argument("-v", "--VERBOSE", type=str, dest="verbose", action="store_true",
+                        help="Optional: Run with VERBOSE debug setting")
     args = parser.parse_args()
+
+    # Update VERBOSE
+    if args.verbose:
+        VERBOSE = True
 
     # Get the appropriate WebDriver
     driver = get_webdriver(args.browser)
@@ -40,6 +52,14 @@ def main():
     driver.get(args.url)
     print(f"Opened {args.url} in {args.browser.capitalize()}")
     print("Page Title:", driver.title)
+
+    # TODO: We need to find the HTML 
+
+    # Page front
+
+    # Page reservation form
+
+    # Submission (this is unknown)
 
     # Exit
     #driver.quit()
